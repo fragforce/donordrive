a33d8424de8445b6f34100993fb99ccd87708d0b
Paulson McIntyre <pmcintyre@salesforce.com>
2022-05-21 20:53:06 -0400

    Changed arg name to be more clear

--- team.go
+++ team.go
@@ -1,24 +1,22 @@
 package donordrive
 
-import "time"
-
 type Team struct {
-	AvatarImageURL     string     `json:"avatarImageURL"`
-	CaptainDisplayName string     `json:"captainDisplayName"`
-	CreatedDateUTC     *time.Time `json:"createdDateUTC"`
-	EventID            int        `json:"eventID"`
-	EventName          string     `json:"eventName"`
-	FundraisingGoal    string     `json:"fundraisingGoal"`
-	IsInviteOnly       bool       `json:"isInviteOnly"`
-	Links              *Links     `json:"links"`
-	Name               string     `json:"name"`
-	NumAwardedBadges   int        `json:"numAwardedBadges"`
-	NumDonations       int        `json:"numDonations"`
-	NumParticipants    int        `json:"numParticipants"`
-	StreamIsLive       bool       `json:"streamIsLive"`
-	SumDonations       float32    `json:"sumDonations"`
-	SumPledges         float32    `json:"sumPledges"`
-	TeamID             int        `json:"teamID"`
+	AvatarImageURL     string  `json:"avatarImageURL"`
+	CaptainDisplayName string  `json:"captainDisplayName"`
+	CreatedDateUTC     string  `json:"createdDateUTC"`
+	EventID            int     `json:"eventID"`
+	EventName          string  `json:"eventName"`
+	FundraisingGoal    string  `json:"fundraisingGoal"`
+	IsInviteOnly       bool    `json:"isInviteOnly"`
+	Links              *Links  `json:"links"`
+	Name               string  `json:"name"`
+	NumAwardedBadges   int     `json:"numAwardedBadges"`
+	NumDonations       int     `json:"numDonations"`
+	NumParticipants    int     `json:"numParticipants"`
+	StreamIsLive       bool    `json:"streamIsLive"`
+	SumDonations       float32 `json:"sumDonations"`
+	SumPledges         float32 `json:"sumPledges"`
+	TeamID             int     `json:"teamID"`
 }
 
 type Links struct {
